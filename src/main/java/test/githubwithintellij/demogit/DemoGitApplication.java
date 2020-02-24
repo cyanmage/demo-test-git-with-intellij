1000af3b9bb3a232e56147e889deee69ad2a8ed8
cyanmage <sahbi.bensaida@yahoo.fr>
2020-02-24 17:31:15 +0100

    "first commit"

---
+++ src/main/java/test/githubwithintellij/demogit/DemoGitApplication.java
@@ -0,0 +1,13 @@
+package test.githubwithintellij.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/githubwithintellij/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package test.githubwithintellij.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
